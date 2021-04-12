cad98ebb971634fb8676c07ee31a23f1870f4c7e
ngodat2002 <73330656+ngodat2002@users.noreply.github.com>
2021-04-13 00:10:53 +0700

    demo

---
+++ Task5/codelean/CodeLeanA.java
@@ -0,0 +1,15 @@
+package codelean;
+
+public class CodeLeanA {
+    public void methodPublic(){
+        methodPublic();
+    }
+    protected void methodProtected(){
+        methodProtected();
+    }
+    void methodDefault(){
+        methodPrivate();
+    }
+    private void methodPrivate(){
+    }
+}
---
+++ Task5/codelean/CodeLeanB.java
@@ -0,0 +1,15 @@
+package codelean;
+
+public class CodeLeanB
+{
+    public static void main(String[] args) {
+        new CodeLeanA().methodPublic();
+        new CodeLeanA().methodProtected();
+        new CodeLeanA().methodDefault();
+    }
+    public void methodPublic(){
+
+    }
+    protected void methodProtected(){}
+    void methodDefault(){}
+}
